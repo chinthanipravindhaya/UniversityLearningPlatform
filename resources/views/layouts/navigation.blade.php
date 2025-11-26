709f3b378f0b0b83646b8fe7c843116aafb639af
chinu1227 <premachandrachinu27gmail.com>
2025-11-26 11:15:43 +0530

    Initial commit

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,48 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary bg-gradient shadow-sm fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand fw-bold ps-2 fs-6" href="#">University Learning Portal</a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav align-items-center">
+
+                <li class="nav-item me-2">
+                    <a class="nav-link" href="#">
+                        <i class="bi bi-search fs-6 text-white" ></i>
+                    </a>
+                </li>
+
+                <li class="nav-item me-3">
+                    <a class="nav-link position-relative" href="#">
+                        <i class="bi bi-chat-dots fs-6 text-white"></i>
+                        <span class="position-absolute top-10 start-75 translate-middle badge rounded-pill bg-danger" style="font-size: 8px; padding: 2px 4px;">0</span>
+                    </a>
+                </li>
+
+                <li class="nav-item me-3">
+                    <a class="nav-link position-relative" href="#">
+                        <i class="bi bi-bell fs-6 text-white"></i>
+
+                        <span class="position-absolute top-10 start-75 translate-middle badge rounded-pill bg-danger" style="font-size: 8px; padding: 2px 4px;">
+                            0
+                        </span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link text-white fs-6" href="#">
+                        <i class="bi bi-person-circle me-1 fs-6 text-white"></i> Chinthani Pravindhaya
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link text-white fs-6" href="#">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
